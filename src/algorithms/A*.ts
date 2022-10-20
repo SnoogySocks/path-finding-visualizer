bd38ee4d5c8886ff0096f3dfff70ea67bbfb8ae4
soutrik-g <soutrik.guhathakurta@gmail.com>
2022-10-20 17:01:12 -0400

    A* start

--- src/algorithms/A*.ts
+++ src/algorithms/A*.ts
@@ -1,6 +1,3 @@
-// implement the A* algorithm
-// https://en.wikipedia.org/wiki/A*_search_algorithm
-
 import { Node } from "../PathFindingVisualizer/Node";
 import { PriorityQueue } from "./util";
 
