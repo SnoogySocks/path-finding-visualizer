5f17ef3be9b8a19fb52ff9471b916146352ec936
fvcci <fongf2@mcmaster.ca>
2023-01-05 23:32:17 -0500

    go back to 721e63374c5652b001a7f68c0b249aa7b06b709d

--- src/PathFindingVisualizer/Grid/useGrid.ts
+++ src/PathFindingVisualizer/Grid/useGrid.ts
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useCallback } from "react";
 import { NodeType } from "../Node";
 
 // import local files
-import { START_END_RATIO, NODE_STATE } from "../../constants";
+import { START_END_COORDS, GRID_SIZE, NODE_STATE } from "../../constants";
 
 interface useGridType {
   grid: NodeType[][];
@@ -13,32 +13,30 @@ interface useGridType {
   clearGridState: (statesToClear: string[], draggedNode: NodeType) => boolean;
 }
 
-const useGrid = (rows: number, cols: number): useGridType => {
+const useGrid = (): useGridType => {
   const [grid, setGrid] = useState<NodeType[][]>([]);
 
-  const initNode = useCallback(
-    (
-      row: number,
-      col: number,
-      start: { row: number; col: number },
-      end: { row: number; col: number }
-    ): NodeType => {
-      let state = "";
-      if (row === start.row && col === start.col) {
-        state = NODE_STATE.START;
-      } else if (row === end.row && col === end.col) {
-        state = NODE_STATE.END;
-      }
+  const initNode = useCallback((row: number, col: number): NodeType => {
+    let state = "";
+    if (
+      row === START_END_COORDS.START_NODE_ROW &&
+      col === START_END_COORDS.START_NODE_COL
+    ) {
+      state = NODE_STATE.START;
+    } else if (
+      row === START_END_COORDS.END_NODE_ROW &&
+      col === START_END_COORDS.END_NODE_COL
+    ) {
+      state = NODE_STATE.END;
+    }
 
-      return {
-        row,
-        col,
-        weight: 1,
-        state,
-      };
-    },
-    []
-  );
+    return {
+      row,
+      col,
+      weight: 1,
+      state,
+    };
+  }, []);
 
   const initNodeFromDOM = (row: number, col: number): NodeType => {
     let state = "";
@@ -57,24 +55,15 @@ const useGrid = (rows: number, cols: number): useGridType => {
   };
 
   const initGrid = useCallback(() => {
-    const start = {
-      row: Math.floor(rows * START_END_RATIO.START.ROW),
-      col: Math.floor(cols * START_END_RATIO.START.COL),
-    };
-    const end = {
-      row: Math.floor(rows * START_END_RATIO.END.ROW),
-      col: Math.floor(cols * START_END_RATIO.END.COL),
-    };
-
-    let grid = new Array(rows);
+    let grid = new Array(GRID_SIZE.ROW_SIZE);
     for (let r = 0; r < grid.length; ++r) {
-      grid[r] = new Array(cols);
+      grid[r] = new Array(GRID_SIZE.COL_SIZE);
       for (let c = 0; c < grid[r].length; ++c) {
-        grid[r][c] = initNode(r, c, start, end);
+        grid[r][c] = initNode(r, c);
       }
     }
     return grid;
-  }, [rows, cols]);
+  }, []);
 
   // Create a new grid with grid[row][col] modified to value
   const setCell = useCallback(
